29805799368ae0ac7a73fd8e48197905eee9735b
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-07-06 10:39:16 -0700

    Swapping.java
    Swapping the values

---
+++ Swapping.java
@@ -0,0 +1,16 @@
+public class Swapping
+{
+    public static void main(String args[])
+    {
+        int a=10,b=20,temp;
+        System.out.println("values of A before swapping "+a);
+    System.out.println("Values of B before Swapping "+b);
+    temp=a;
+    a=b;
+    b=temp;
+    System.out.println("Values of A After swapping "+a);
+    System.out.println("avlues of B After Swapping "+b);
+    
+
+    }
+}
