9c45b6ecb10f1c05c593595938932de5941088d4
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-07-08 10:22:01 -0700

    Table.java
    Create the table

---
+++ Table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Table
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter to Value ");
+        int n=sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+ " * " +i+ " = "+n*i);
+        }
+    }
+}
