0319dae3df13f3838552f5098fa83739ae593297
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-07-16 08:42:08 -0700

    Array3.java
    Write a program sum of values of an  array?

---
+++ Array3.java
@@ -0,0 +1,14 @@
+public class Array3 
+{
+    public static void main(String args[])
+    {
+        int a[]={1,2,3,4,5};
+        int sum=0;
+        System.out.println("Value sof an array ");
+        for(int i=0;i<5;i++)
+        {
+            sum=sum+a[i];
+        }
+        System.out.println("Sum of the values of an array:-" +sum);
+    }    
+}
