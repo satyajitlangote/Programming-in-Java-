7de381090b7edccf84a88a4c6d38f221cad7743d
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-07-16 09:01:12 -0700

    Array4.java
    Write a program accept values of an array and print them

---
+++ Array4.java
@@ -0,0 +1,23 @@
+ import java.util.Scanner;
+ public class Array4
+ {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i;
+        System.out.println("Enter the size of an array ");
+        int size=sc.nextInt();
+        int a[]=new int[size];
+        System.out.println("Enter the element in the array");
+        for(i=0;i<
+        size;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        System.out.println("Arry Elemnt is"+size);
+        for(i=0;i<size;i++);
+        {
+            System.out.println(a[i]+"   ");
+        }
+    }
+ }
