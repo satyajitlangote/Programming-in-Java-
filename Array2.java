75eea425a3c48d70d77f04b6b63c87bbaf2dd9a3
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-07-16 08:34:25 -0700

    Array2.java
    Write a program to print Square and cubes of values ?

---
+++ Array2.java
@@ -0,0 +1,18 @@
+public class Array2 
+{
+    public static void main(String args[])
+    {
+        int a[]={1,2,3,4,5};
+        int i;
+        System.out.println("Value of an array");
+        for(i=0;i<5;i++)
+        {
+            System.out.println(a[i]);
+        }
+        System.out.println("Square and cube of values of array");
+        for(i=0;i<=4;i++)
+        {
+            System.out.println(a[i]+"  "+a[i]*a[i]+"  "+a[i]*a[i]+a[i]);
+        }
+    }
+}
