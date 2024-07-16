7d742e667b497c80142705bcccba21cce362a7bc
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-07-16 08:13:49 -0700

    Array1.java
    Write a program values of an array in reverse order?

---
+++ Array1.java
@@ -0,0 +1,17 @@
+public class Array1
+{
+    public static void main(String args[])
+    {
+        int a[]={1,2,3,4,5};
+        System.out.println("Value of the array");
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(a[i]);
+        }
+        System.out.println("Value of an array reverse order ");
+        for(int i=41;i>=0;i--)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
