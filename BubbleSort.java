bd61a3a829854c243351963739d11b8454e93d56
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-07-19 09:55:28 -0700

    Write a program bubble sort?

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-public class BubbleSort
+public class BubbleSort 
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int i,j,n,temp;
-        System.out.println("Enter size of a array");
+        System.out.println("Enter size of array");
         n=sc.nextInt();
         int a[]=new int[10];
         System.out.println("Enter array values ");
@@ -13,29 +13,28 @@ public class BubbleSort
         {
             a[i]=sc.nextInt();
         }
-        System.out.println("Display the element of an array ");
+        System.out.println("Dispaly the element of an array ");
         for(i=0;i<=n;i++)
         {
-            System.out.print(a[i]+ "   ");
+            System.out.println(a[i]+" ");
         }
         System.out.println();
-        // Bubble Sort 
         for(i=0;i<=n;i++)
         {
             for(j=0;j<=n;j++)
             {
-                if(a[i] < a[j])
+                if(a[i]<a[j])
                 {
                     temp=a[i];
                     a[i]=a[j];
                     a[j]=temp;
                 }
             }
-        }
-        System.out.println("Bubble Sorted elements");
+        }        
+        System.out.println("Bubble sorted elements are");
         for(i=0;i<=n;i++)
         {
-            System.out.print(a[i]+ "   ");
+            System.out.println(a[i]+" ");
         }
-    }
+    }    
 }
