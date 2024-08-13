6ef9f1ab4f69edb1956860708a2c99d8767b3f12
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-08-13 11:18:14 -0700

    Add files via upload

---
+++ Traingle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Traingle
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Height of traingle");
+        double h=sc.nextDouble();
+        System.out.println("Enter Base of traingle");
+        double b=sc.nextDouble();
+        double a=0.5*h*b;
+        System.out.println("Area of Traingle\n " +a);
+    }
+}
