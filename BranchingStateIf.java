9334b85b66a44ea3d954fb1a388243810db4fbf6
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-09-10 19:06:35 +0530

    Branching if Statement in java programing

---
+++ BranchingStateIf.java
@@ -0,0 +1,11 @@
+public class BranchingStateIf
+{
+    public static void main(String args[])
+    {
+        int x=20;
+        if(x<30)
+        {
+            System.out.println("This is if statement "+x);
+        }
+    }
+}
