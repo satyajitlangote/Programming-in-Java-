bbe9cae4beb843aa19028594299b81e5538ff604
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-09-10 19:14:31 +0530

    If else program in java

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+public class IfElse
+{
+    public static void main(String args[])
+    {
+        int x=20;
+        if(x < 30 )
+        {
+            System.out.println("This is if statement ");
+        }
+        else 
+        {
+            System.out.println("This is else statement ");
+        }
+    }
+}
