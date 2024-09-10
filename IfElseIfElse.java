2f48d0f12a99151c375c877d48df21595dac983e
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-09-10 19:43:53 +0530

    IfElse else if statement of program in java

---
+++ IfElseIfElse.java
@@ -0,0 +1,23 @@
+class IfElseIfElse
+{
+    public static void main(String args[])
+    {
+        int x=25;
+        if(x== 10)
+        {
+            System.out.println("value of x is 10 ");
+        }
+        else 
+        if(x == 20)
+        {
+            System.out.println("value of x is 20");
+        }
+        else
+        if(x == 30)
+        {
+            System.out.println("The Value of x is 30 ");
+        }
+        else{
+            System.out.println("This is else statement ");        }
+    }
+}
