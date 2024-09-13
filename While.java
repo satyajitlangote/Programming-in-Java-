da67760afc8cc96abde83b4b9277c640e4b7accc
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-09-13 16:43:21 +0530

    While loop program in java

---
+++ While.java
@@ -0,0 +1,13 @@
+class While
+{
+    public static void main(String args[])
+    {
+        int x=20;
+        while(x<40)
+        {
+            System.out.println("Value of x is "+x);
+            x++;
+            System.out.println("\n ");
+        }
+    }
+}
