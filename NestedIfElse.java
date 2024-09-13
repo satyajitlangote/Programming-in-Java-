f7626b974da2e9526be0156ced8a499490050a21
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-09-13 15:51:48 +0530

    Nested if else statement java program

---
+++ NestedIfElse.java
@@ -0,0 +1,15 @@
+class NestedIfElse
+{
+    public static void main(String args[])
+    {
+        int x=20;
+        int y=30;
+        if(x==20)
+        {
+            if(y == 30)
+            {
+                System.out.println("X=20 and Y= 30");
+            }
+        }
+    }
+}
