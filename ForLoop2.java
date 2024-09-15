c9efa5c66a1b753de295e985f77306bbd664926a
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-09-15 09:08:18 +0530

    Enhanced for loop in Java Program

---
+++ ForLoop2.java
@@ -0,0 +1,19 @@
+public class ForLoop2
+{
+    public static void main(String args[])
+    {
+        int[] numbers= {10,20,30,40,50};
+        for(int x : numbers)
+        {
+            System.out.println(x);
+            System.out.println(",");
+        }
+        System.out.println(" \n ");
+        String [] names = {"james","larry","tom","lacy"};
+        for(String name : names)
+        {
+            System.out.println(name);
+            System.out.println("  ");
+        }
+    }
+}
