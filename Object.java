f83926c0f5142970496a8ac3b9d7c4e37ba51a95
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-09-17 21:34:00 +0530

    object in java programing

---
+++ Object.java
@@ -0,0 +1,12 @@
+public class Object {
+    public Object(String name)
+    {
+        System.out.println("Passed Name is "+name);
+    }
+    public static void main(String args[])
+    {
+        Object myobject = new Object("tommy");
+    }
+    
+
+}
