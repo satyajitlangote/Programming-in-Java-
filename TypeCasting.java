c5cbbc629e5d2474b4f51ae129a2ff92d4573be0
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-10-01 23:58:57 +0530

    Type Casting Program in java

---
+++ TypeCasting.java
@@ -0,0 +1,14 @@
+public class TypeCasting 
+{  
+public static void main(String[] args)  
+{  
+int x = 7;  
+  
+long y = x;  
+ 
+float z = y;  
+System.out.println("Before conversion, int value "+x);  
+System.out.println("After conversion, long value "+y);  
+System.out.println("After conversion, float value "+z);  
+}  
+} 
