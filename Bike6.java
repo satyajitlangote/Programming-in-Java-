0e813986e45b76bbd694e76e24f3b1d7891f7b44
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2024-10-08 22:51:32 +0530

    Java Runtime Polymorphism with data member  Rule: Runtime polymorphism can't be achieved by  data members.

---
+++ Bike6.java
@@ -0,0 +1,18 @@
+ 
+class Bike6
+{   
+ int speedlimit=90;   
+}   
+ 
+class Honda3 extends Bike 
+{   
+ int speedlimit=150;   
+   
+ public static void main(String ar
+ gs[]) 
+{   
+  Bike obj=new Honda3();   
+  System.out.println(obj.speedlimi
+ t);//90   
+}   
+} 
